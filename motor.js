d7b135df9cb92084caf0e735d60f93563655f565
pauletik <pauletik@nobeboaguasolococacola.com>
2025-04-07 19:04:43 +0200

    marcador completado

--- motor.js
+++ motor.js
@@ -1,3 +1,18 @@
+function sumarPuntos(suma) {
+
+    if (!suma) {
+        suma = 1;
+    }
+
+    document.getElementById("puntos").innerHTML = parseInt(document.getElementById("puntos").innerHTML) + suma;
+}
+
+function detectarDerrota() {
+    if (parseInt(document.getElementById("puntos").innerHTML) > 0) {
+        return false;
+    }
+    return true;
+}
 document.addEventListener('DOMContentLoaded', () => {
     const personaje = document.getElementById('personaje');
     let posicion = 0;
